560db39a47ee17ef738bcefa1fd661285796b0e4
Cristian Pascumal <cristian.pascumal@gmail.com>
2021-09-08 12:05:20 -0500

    plantillaReact

---
+++ front_end/src/app/views/register/register.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: 'register.component.html'
+})
+
+
+export class RegisterComponent {
+
+  registro = true;
+  edicion = false;
+
+  constructor() { }
+
+}
