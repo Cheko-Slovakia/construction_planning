0b9abbb89bfdc879c6729983afe58974b5ae73db
Cristian Pascumal <cristian.pascumal@gmail.com>
2021-09-19 11:31:43 -0500

    Adicionando listado de trabajadores

--- front_end/src/app/views/listarTrabajadores/listarTrabajadores.component.ts
+++ front_end/src/app/views/listarTrabajadores/listarTrabajadores.component.ts
@@ -1,22 +1,97 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { Trabajador } from '../../../models/Trabajador';
 import { TrabajadorService } from '../../services/TrabajadorService';
+
+//Interfaz de los trabajadores que se mostrarán 
+declare interface trabajadoresTabla {
+  cedula: string,
+  nombre: string,
+  apellido: string,
+  celular: string,
+  cargo: string
+
+}
 
 @Component({
   selector: 'app-listartrabajadores',
   templateUrl: './listarTrabajadores.component.html',
   styleUrls: ['./listarTrabajadores.component.scss']
 })
+
+
+
+
 export class ListarTrabajadoresComponent implements OnInit {
+
+  private trabajadores: trabajadoresTabla[] = [];//Trabajadores
+  private columnasTrabajadores: string[] = ['nombre', 'apellido', 'celular', 'cargo', 'editar'];//Columnas a mostrar en la tabla
+  private dataSourceTrabajadores: MatTableDataSource<trabajadoresTabla>
+
+
+  private trabajadorAux: trabajadoresTabla = {
+    cedula: null,
+    nombre: null,
+    apellido: null,
+    celular: null,
+    cargo: null
+  }
+
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
+
+
   constructor(private trabajadoresService: TrabajadorService, private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    //Se genera el menu
+    console.log('Ola');
+    this.generarMenuTrabajadores();
+  }
+
+
+  generarMenuTrabajadores() {
+    
+    this.trabajadoresService.obtenerTrabajadores().subscribe(
+      (response: Trabajador[]) => {
+
+        console.log('Ola');
+        
+        console.log(response);
+        
+
+        response.forEach(trabajador => {
+          this.trabajadorAux = {
+            cedula: trabajador.numero_cedula,
+            nombre: trabajador.nombre,
+            apellido: trabajador.apellido,
+            celular: trabajador.numero_celular,
+            cargo: trabajador.cargo
+          }
+          this.trabajadores.push(this.trabajadorAux)
+        })
+
+        //Datasource para la tabla y su correspondiende sorter y paginator
+        this.dataSourceTrabajadores = new MatTableDataSource<trabajadoresTabla>(this.trabajadores)
+        this.dataSourceTrabajadores.sort = this.sort;
+        this.dataSourceTrabajadores.paginator = this.paginator;
+      }
+    )
+  }
+
+   //Filtrado
+   applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceTrabajadores.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSourceTrabajadores.paginator) {
+      this.dataSourceTrabajadores.paginator.firstPage();
+    }
   }
 
 }
